dc517939ec455b6436830794cb884327ea038e83
David Vasquez Rios <davidvasquez235678@correo.itm.edu.co>
2023-05-20 23:51:12 -0500

    all to english

--- ViewModels/CalculatorViewModel.cs
+++ ViewModels/CalculatorViewModel.cs
@@ -33,6 +33,7 @@ namespace Calculadora.ViewModels
         public ICommand OperatorButtonCommand { get; }
         public ICommand EqualButtonCommand { get; }
         public ICommand ClearButtonCommand { get; }
+        public ICommand DeleteButtonCommand { get; }
 
         public CalculatorViewModel()
         {
@@ -40,6 +41,16 @@ namespace Calculadora.ViewModels
             OperatorButtonCommand = new Command<string>(OnOperatorButtonClicked);
             EqualButtonCommand = new Command(OnEqualButtonClicked);
             ClearButtonCommand = new Command(OnClearButtonClicked);
+            DeleteButtonCommand = new Command(OnDeleteButtonClicked);
+        }
+        private void OnDeleteButtonClicked()
+        {
+            if (currentOperation == "0")
+            {
+                currentOperation = "";
+            }
+           
+            CurrentOperation = currentOperation.Remove(currentOperation.Length - 1);
         }
 
         private void OnNumberButtonClicked(string number)
@@ -84,7 +95,7 @@ namespace Calculadora.ViewModels
             CurrentOperation = "";
         }
 
-        public static double Calculate(List<string> operation)
+        private static double Calculate(List<string> operation)
         {
             Stack<double> numbers = new Stack<double>();
             Stack<string> operators = new Stack<string>();
@@ -102,7 +113,7 @@ namespace Calculadora.ViewModels
                         double segundoNumero = numbers.Pop();
                         double primerNumero = numbers.Pop();
                         string operador = operators.Pop();
-                        double resultadoOperacion = RealizarOperacion(primerNumero, operador, segundoNumero);
+                        double resultadoOperacion = doOperation(primerNumero, operador, segundoNumero);
                         numbers.Push(resultadoOperacion);
                     }
 
@@ -112,42 +123,42 @@ namespace Calculadora.ViewModels
 
             while (operators.Count > 0)
             {
-                double segundoNumero = numbers.Pop();
-                double primerNumero = numbers.Pop();
-                string operador = operators.Pop();
-                double resultadoOperacion = RealizarOperacion(primerNumero, operador, segundoNumero);
-                numbers.Push(resultadoOperacion);
+                double secondNumber = numbers.Pop();
+                double firstNumber = numbers.Pop();
+                string symbol = operators.Pop();
+                double operationResult = doOperation(firstNumber, symbol, secondNumber);
+                numbers.Push(operationResult);
             }
 
             return numbers.Pop();
         }
 
-        public static bool IsNumber(string texto)
+        private static bool IsNumber(string texto)
         {
-            double numero;
-            return double.TryParse(texto, out numero);
+            double number;
+            return double.TryParse(texto, out number);
         }
 
-        public static double RealizarOperacion(double numero1, string operador, double numero2)
+        private static double doOperation(double number1, string symbol, double number2)
         {
-            switch (operador)
+            switch (symbol)
             {
                 case "+":
-                    return numero1 + numero2;
+                    return number1 + number2;
                 case "-":
-                    return numero1 - numero2;
+                    return number1 - number2;
                 case "*":
-                    return numero1 * numero2;
+                    return number1 * number2;
                 case "/":
-                    return numero1 / numero2;
+                    return number1 / number2;
                 default:
                     throw new ArgumentException("Operador inválido");
             }
         }
 
-        public static bool HaveMorePriority(string operador1, string operador2)
+        private static bool HaveMorePriority(string operator1, string operator2)
         {
-            if ((operador2 == "*" || operador2 == "/") && (operador1 == "+" || operador1 == "-"))
+            if ((operator2 == "*" || operator2 == "/") && (operator1 == "+" || operator1 == "-"))
             {
                 return false;
             }
