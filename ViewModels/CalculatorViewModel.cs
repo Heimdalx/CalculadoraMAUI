69b9c9fa73d15cf3dde41d28544ad578c2e90d33
David Vasquez Rios <davidvasquez235678@correo.itm.edu.co>
2023-05-20 20:40:14 -0500

    logic done

--- ViewModels/CalculatorViewModel.cs
+++ ViewModels/CalculatorViewModel.cs
@@ -1,12 +1,90 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows.Input;
 
 namespace Calculadora.ViewModels
 {
-    internal class CalculatorViewModel
+    public class CalculatorViewModel : BindableObject
     {
+        private string currentNumber = "";
+        private string operation = "";
+        private double result = 0;
+
+        public string CurrentNumber
+        {
+            get { return currentNumber; }
+            set
+            {
+                currentNumber = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand NumberButtonCommand { get; }
+        public ICommand OperatorButtonCommand { get; }
+        public ICommand EqualButtonCommand { get; }
+        public ICommand ClearButtonCommand { get; }
+
+        public CalculatorViewModel()
+        {
+            NumberButtonCommand = new Command<string>(OnNumberButtonClicked);
+            OperatorButtonCommand = new Command<string>(OnOperatorButtonClicked);
+            EqualButtonCommand = new Command(OnEqualButtonClicked);
+            ClearButtonCommand = new Command(OnClearButtonClicked);
+        }
+
+        private void OnNumberButtonClicked(string number)
+        {
+            if (currentNumber == "0" && number != ".")
+            {
+                currentNumber = "";
+            }
+
+            currentNumber += number;
+            CurrentNumber = currentNumber;
+        }
+
+        private void OnOperatorButtonClicked(string op)
+        {
+            if (!string.IsNullOrEmpty(currentNumber))
+            {
+                operation = op;
+                result = double.Parse(currentNumber);
+                currentNumber = "";
+            }
+        }
+
+        private void OnEqualButtonClicked()
+        {
+            if (!string.IsNullOrEmpty(currentNumber) && !string.IsNullOrEmpty(operation))
+            {
+                double secondNumber = double.Parse(currentNumber);
+                switch (operation)
+                {
+                    case "+":
+                        result += secondNumber;
+                        break;
+                    case "-":
+                        result -= secondNumber;
+                        break;
+                    case "×":
+                        result *= secondNumber;
+                        break;
+                    case "÷":
+                        result /= secondNumber;
+                        break;
+                }
+
+                currentNumber = result.ToString();
+                CurrentNumber = currentNumber;
+                operation = "";
+            }
+        }
+
+        private void OnClearButtonClicked()
+        {
+            currentNumber = "";
+            operation = "";
+            result = 0;
+            CurrentNumber = "0";
+        }
     }
 }
